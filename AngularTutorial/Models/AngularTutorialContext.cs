a82057fbfbb65225ba8f63b44dbf880773a20087
mahbubhasan83 <mahbubhasan83@yahoo.com>
2014-07-13 20:07:58 +0600

    AngularTutorialContext, Web.Release.config
    Changed AngularTutorialContext, Web.Release.config

--- AngularTutorial/Models/AngularTutorialContext.cs
+++ AngularTutorial/Models/AngularTutorialContext.cs
@@ -17,6 +17,12 @@ namespace AngularTutorial.Models
         {
         }
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<AngularTutorialContext, Migrations.Configuration>());
+        }
+
         public DbSet<TodoItem> TodoItems { get; set; }
     }
 }
